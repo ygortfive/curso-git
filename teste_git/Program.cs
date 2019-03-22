cc461253dd523552735fea91d53de3781985e2ef
Ygor Mattos <ygormattos.b@gmail.com>
2019-03-22 18:51:03 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ teste_git/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace teste_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testando git");
+        }
+    }
+}
